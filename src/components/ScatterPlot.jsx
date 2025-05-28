ee27de5151453aa5b68e2be9ae4c92dd45b7f826
DESKTOP-MOCM04R\Jeonghwan <bangjh0730@yonsei.ac.kr>
2025-05-29 05:05:26 +0900

    Change tooltip to hover

--- src/components/ScatterPlot.jsx
+++ src/components/ScatterPlot.jsx
@@ -1,11 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
 
 /**
  * ScatterPlot
  * @param {Array} items - Array of data points, each with { x, y, speed, name, type, color? }
  * @param {Object} selectedPokemon - The selected Pokémon object (must have .stat.spe)
- * @param {string} selectedItem - Name of the currently selected Pokémon (for tooltip)
+ * @param {string} selectedItem - Name of the currently selected Pokémon (for detail)
  * @param {function} setSelectedItem - Setter for selectedItem
  * @param {boolean} showMove - Whether to show move name in tooltip
  */
@@ -17,6 +17,7 @@ export default function ScatterPlot({
   showMove = false,
 }) {
   const ref = useRef();
+  const [hoveredItem, setHoveredItem] = useState(null);
 
   useEffect(() => {
     const svg = d3.select(ref.current);
@@ -32,22 +33,12 @@ export default function ScatterPlot({
     const marginLeft = 50;
     const marginRight = 20;
 
-    // Scales
-    const x = d3
-      .scaleLinear()
-      .domain([0, 5])
-      .range([marginLeft, width - marginRight]);
-
-    const y = d3
-      .scaleLinear()
-      .domain([0, 5])
-      .range([height - marginBottom, marginTop]);
+    const x = d3.scaleLinear().domain([0, 5]).range([marginLeft, width - marginRight]);
+    const y = d3.scaleLinear().domain([0, 5]).range([height - marginBottom, marginTop]);
 
     svg.attr("width", width).attr("height", height);
 
-    // X Axis
-    svg
-      .append("g")
+    svg.append("g")
       .attr("transform", `translate(0, ${height - marginBottom})`)
       .call(d3.axisBottom(x).ticks(6))
       .append("text")
@@ -58,9 +49,7 @@ export default function ScatterPlot({
       .attr("font-size", 14)
       .text("Hits to KO Selected Pokémon");
 
-    // Y Axis
-    svg
-      .append("g")
+    svg.append("g")
       .attr("transform", `translate(${marginLeft}, 0)`)
       .call(d3.axisLeft(y).ticks(6))
       .append("text")
@@ -72,20 +61,16 @@ export default function ScatterPlot({
       .attr("font-size", 14)
       .text("Hits each Pokémon can endure from selected move");
 
-    // Plot points
     const pointsGroup = svg.append("g").attr("class", "datapoints");
 
-    pointsGroup
-      .selectAll("circle")
+    pointsGroup.selectAll("circle")
       .data(items)
       .join("circle")
       .attr("cx", (d) => x(d.x))
       .attr("cy", (d) => y(d.y))
       .attr("r", 5)
       .attr("fill", (d) =>
-        d.speed > selectedPokemon.stat.spe
-          ? "green"
-          : "red"
+        d.speed > selectedPokemon.stat.spe ? "green" : "red"
       )
       .attr("stroke", (d) =>
         d.name === selectedItem ? "#222" : "white"
@@ -94,61 +79,61 @@ export default function ScatterPlot({
       .style("cursor", "pointer")
       .on("click", (event, d) => {
         setSelectedItem(d.name === selectedItem ? null : d.name);
+      })
+      .on("mouseenter", (event, d) => {
+        setHoveredItem(d);
+      })
+      .on("mouseleave", () => {
+        setHoveredItem(null);
       });
 
     // Tooltip group
     svg.selectAll(".tooltip-group").remove();
     const tooltipGroup = svg.append("g").attr("class", "tooltip-group");
 
-    // Draw tooltip for selected item
-    items.forEach((d) => {
-      if (d.name === selectedItem) {
-        const tx = x(d.x);
-        const ty = y(d.y) - 30;
+    if (hoveredItem) {
+      const tx = x(hoveredItem.x);
+      const ty = y(hoveredItem.y) - 30;
 
-        const tooltip = tooltipGroup
-          .append("g")
-          .attr("transform", `translate(${tx}, ${ty})`);
+      const tooltip = tooltipGroup
+        .append("g")
+        .attr("transform", `translate(${tx}, ${ty})`);
 
-        // Name
-        const nameText = tooltip
+      const nameText = tooltip
+        .append("text")
+        .text(hoveredItem.name)
+        .attr("text-anchor", "middle")
+        .attr("font-size", 13)
+        .attr("fill", "white")
+        .attr("y", 0)
+        .style("font-weight", "bold");
+
+      let moveText = null;
+      if (showMove && hoveredItem.bestMove) {
+        moveText = tooltip
           .append("text")
-          .text(d.name)
+          .text(hoveredItem.bestMove)
           .attr("text-anchor", "middle")
-          .attr("font-size", 13)
+          .attr("font-size", 11)
           .attr("fill", "white")
-          .attr("y", 0)
-          .style("font-weight", "bold");
-
-        // Move (optional)
-        let moveText;
-        if (showMove && d.bestMove) {
-          moveText = tooltip
-            .append("text")
-            .text(d.bestMove)
-            .attr("text-anchor", "middle")
-            .attr("font-size", 11)
-            .attr("fill", "white")
-            .attr("y", 16);
-        }
-
-        // Get bounding boxes
-        const nameBBox = nameText.node().getBBox();
-        const moveBBox = moveText ? moveText.node().getBBox() : { width: 0, height: 0 };
-        const boxWidth = Math.max(nameBBox.width, moveBBox.width) + 12;
-        const boxHeight = nameBBox.height + (moveText ? moveBBox.height : 0) + 12;
-
-        tooltip
-          .insert("rect", "text")
-          .attr("x", -boxWidth / 2)
-          .attr("y", -nameBBox.height)
-          .attr("width", boxWidth)
-          .attr("height", boxHeight)
-          .attr("rx", 5)
-          .attr("fill", "black");
+          .attr("y", 16);
       }
-    });
-  }, [items, selectedItem, setSelectedItem, showMove, selectedPokemon]);
+
+      const nameBBox = nameText.node().getBBox();
+      const moveBBox = moveText ? moveText.node().getBBox() : { width: 0, height: 0 };
+      const boxWidth = Math.max(nameBBox.width, moveBBox.width) + 12;
+      const boxHeight = nameBBox.height + (moveBBox ? moveBBox.height : 0) + 12;
+
+      tooltip
+        .insert("rect", "text")
+        .attr("x", -boxWidth / 2)
+        .attr("y", -nameBBox.height - 1.5)
+        .attr("width", boxWidth)
+        .attr("height", boxHeight)
+        .attr("rx", 5)
+        .attr("fill", "black");
+    }
+  }, [items, selectedItem, hoveredItem, setSelectedItem, showMove, selectedPokemon]);
 
   return <svg ref={ref} />;
 }
